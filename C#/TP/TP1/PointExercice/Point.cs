28f49035db435f5849eeff1543f8bf7f087e8929
divino <61043665+divinoPV@users.noreply.github.com>
2020-09-27 21:37:36 +0200

    Add files via upload

---
+++ C#/TP/TP1/PointExercice/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointExercice
+{
+    public class Point
+    {
+        // Attr
+        private int abscisse;
+        private int ordonne;
+
+        // Constructor
+        public Point(int abscisse, int ordonne)
+        {
+            this.abscisse = abscisse;
+            this.ordonne = ordonne;
+        }
+
+        // Propriété
+        public int Abscisse
+        {
+            get => abscisse;
+            set => abscisse = value;
+        }
+        public int Ordonne
+        {
+            get => ordonne;
+            set => ordonne = value;
+        }
+
+        // Méthode
+        public string affiche()
+        {
+            return "L'abscisse est " + this.abscisse + " et l'ordonné est " + this.ordonne;
+        }
+
+        public void deplace(int abscisse, int ordonne)
+        {
+            this.abscisse += abscisse;
+            this.ordonne += ordonne;
+        }
+    }
+}
